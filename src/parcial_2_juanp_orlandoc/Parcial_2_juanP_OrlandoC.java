3d00d0f3026984b06afa088fa5b528648c0373c5
sport <sport@DESKTOP-6JDHE76>
2021-03-25 18:26:39 -0500

    buenas noches camaradas.

---
+++ src/parcial_2_juanp_orlandoc/Parcial_2_juanP_OrlandoC.java
@@ -0,0 +1,16 @@
+
+package parcial_2_juanp_orlandoc;
+
+
+public class Parcial_2_juanP_OrlandoC {
+
+
+    public static void main(String[] args) {
+        
+        
+        //prueba a ver si te llega el mensaje Perez
+        
+        
+    }
+    
+}

f8910f88ffbaf24f23222f08e51c61d7141f6d42
Termi <Termi@DESKTOP-TKNBB6C>
2021-03-25 22:15:26 -0500

    Se añadieron los respectivos getters y setters con un constructor y los metodos imprimir y capturar se volvieros abstractos

--- src/modelo/factura.java
+++ src/modelo/factura.java
@@ -2,11 +2,34 @@
 package modelo;
 
 
-public class factura {
+public abstract class factura {
     
     protected int numero;
     protected double descuento;
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
     
+    public abstract String capturar(); //mucho ojo, puede que no agarre todos los tipos de datos por ser un metodo tipo String 
     
+    public abstract String imprimir(); //mucho ojo, puede que no agarre todos los tipos de datos por ser un metodo tipo String 
+    
+    public factura(int numero, double descuento) {
+        this.numero = numero;
+        this.descuento = descuento;
+    }
     
 }

f67ded33c060f06c6134f47d35c18a6efd3786f8
sport <sport@DESKTOP-6JDHE76>
2021-03-25 21:33:10 -0500

    se agregaron las clases de la carpeta modelo

---
+++ src/modelo/aparato.java
@@ -0,0 +1,58 @@
+
+package modelo;
+
+
+public class aparato {
+    
+    private double precio;
+    private int voltaje;
+    private double descuento;
+
+    public aparato(double precio, int voltaje, double descuento) {
+        this.precio = precio;
+        this.voltaje = voltaje;
+        this.descuento = descuento;
+    }
+    
+    
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getVoltaje() {
+        return voltaje;
+    }
+
+    public void setVoltaje(int voltaje) {
+        this.voltaje = voltaje;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+    
+    public double calcularDescuento (){ 
+        
+        if (descuento <= 0){
+            return precio;
+        } else {
+        double desc = descuento/100;
+        precio = precio / desc;
+        
+        return precio;
+        }
+        
+
+    }
+    
+    
+}
